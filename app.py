7e52304257ae1f905bdcfabdc10c0ec966741765
Kaionobre <kaionobrega00@gmail.com>
2023-05-23 17:51:28 -0300

    Eu sim eu sim

--- app.py
+++ app.py
@@ -1,3 +1,40 @@
 from flask import Flask, jsonify, request
 
-print('oi')
+app = Flask(__name__)
+
+livros = [
+    {
+        'id': 1,
+        'titulo': 'O senhor dos Aneis - A Sociedade do Anel',
+        'autor': 'J.R.R Tolkien', 
+        
+    },
+    
+    {
+        'id': 2,
+        'titulo': 'Harry Potter e a Pedra Filosofal',
+        'autor': 'J.K Howling', 
+        
+    },
+    
+    {
+        'id': 3,
+        'titulo': 'James Clear',
+        'autor': 'Ábitos Atômicos', 
+        
+    },
+]
+
+# Consultar(todos)
+
+@app.route('/livros')
+
+def obter_livros():
+    return jsonify(livros)
+    
+    
+# Consultar(id)    
+# Editar    
+# Excluir
+
+app.run(port=5000,host='localhost',debug=True)
